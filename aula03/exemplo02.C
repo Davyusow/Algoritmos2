8c05c823ec544794fc56efc278c22784a2d54226
Davyusow <dayvsonf27@gmail.com>
2024-11-20 16:03:54 -0300

    Ajustes rápidos e fim da aula03

--- aula03/exemplo02.C
+++ aula03/exemplo02.C
@@ -4,6 +4,17 @@
 int main(void)
 {
     int x[3];
+    x[0]= 1 ;
+    x[1]= 2 ;
+    x[2]= 7 ;
     
+    int* y= (int*) malloc(sizeof(int)*3);
+    y[0] = 4;
+    y[1] = 5;
+    y[2] = 6;
+
+    *y=*x;
+    free(y);
+
     return 0;
 }
