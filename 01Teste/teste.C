1c970845ad508437211519c675c636d535ecc734
Davyusow <dayvsonf27@gmail.com>
2024-11-20 15:23:12 -0300

    Finalmente deu certo

---
+++ 01Teste/teste.C
@@ -0,0 +1,9 @@
+#include "stdio.h"
+
+int main(void){
+
+    printf("Se por algum acaso eu estiver lendo esta mensagem \n");
+    printf("Quer dizer que finalmente o upload deu certo!");
+
+    return 0;
+}
